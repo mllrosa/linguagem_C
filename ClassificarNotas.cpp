af8490dee100b86764291b469a2320ab7598042d
ROSA <mllr8sa@gmail.com>
2026-09-01 17:48:48 -0300

    Atualizar o ClassificarNotas.cpp

--- ClassificarNotas.cpp
+++ ClassificarNotas.cpp
@@ -1,61 +1,93 @@
 /*
-	Name: ClassificarNotas.cpp
-	Author: Rosa Marcella
-	Date: 31/08/26 11:51
-	Description: 
+        Name: ClassificarNotas.cpp
+        Author: Rosa Marcella
+        Date: 31/08/26 11:51
+        Description: 
 */
-
-//4) (Desafio) Um professor, muito legal, fez 3 provas durante um semestre mas só vai levar em conta as duas notas mais altas para calcular a média.
-//Faça uma aplicação em C que peça o valor das 3 notas, passepara uma função e mostre como seria a média comessas 3 provas, a média com as 2 notas mais altas,
-// bem como sua nota mais altaesuanota mais baixa. 
-
-
 
 
 # include <stdio.h>
 
-//Seção de Prototipação
+//SeÃ§Ã£o de PrototipaÃ§Ã£o
 void ClassificarNotas(float *nota1, float *nota2, float *nota3, float *PrimeiraNota, float *SegundaNota, float *MediaPrimeiraSegundaNota, float *MediaTotal, float *TerceiraNota);
 
 int main()
 {
-	float nota1, nota2, nota3;
-	nota1 = nota2 = nota3 = 0.0;
-	
-	float PrimeiraNota,	SegundaNota, MediaPrimeiraSegundaNota, MediaTotal, TerceiraNota;
-	PrimeiraNota = SegundaNota = MediaPrimeiraSegundaNota = MediaTotal = TerceiraNota = 0.0;
-	
-	printf("Digite a nota da sua primeira prova: ");
-	scanf("%f", &nota1);
-	
-	printf("Digite a nota da sua segunda prova: ");
-	scanf("%f", &nota2);
-	
-	printf("Digite a nota da sua terceira prova: ");
-	scanf("%f", &nota3);
-	
-	ClassificarNotas(float *nota1, float *nota2, float *nota3, float *PrimeiraNota,	float *SegundaNota, float *MediaPrimeiraSegundaNota, float *MediaTotal, float *TerceiraNota);
-	
-	printf("Média com 3 provas: %f", MediaTotal);
-	
+        float nota1, nota2, nota3;
+        nota1 = nota2 = nota3 = 0.0;
+
+        float PrimeiraNota,        SegundaNota, MediaPrimeiraSegundaNota, MediaTotal, TerceiraNota;
+        PrimeiraNota = SegundaNota = MediaPrimeiraSegundaNota = MediaTotal = TerceiraNota = 0.0;
+
+        printf("Digite a nota da sua primeira prova: ");
+        scanf("%f", &nota1);
+
+        printf("Digite a nota da sua segunda prova: ");
+        scanf("%f", &nota2);
+
+        printf("Digite a nota da sua terceira prova: ");
+        scanf("%f", &nota3);
+
+        ClassificarNotas( &nota1, &nota2, &nota3, &PrimeiraNota, &SegundaNota, &MediaPrimeiraSegundaNota, &MediaTotal, &TerceiraNota);
+
+        printf("\nMÃ©dia com 3 provas: %.2f", MediaTotal);
+        printf("\nMÃ©dia com as duas notas mais altas: %.2f", MediaPrimeiraSegundaNota);
+        printf("\nSua nota mais alta: %.2f", PrimeiraNota);
+        printf("\nSua nota mais baixa: %.2f", TerceiraNota);
 }
 
 void ClassificarNotas(float *nota1, float *nota2, float *nota3, float *PrimeiraNota, float *SegundaNota, float *MediaPrimeiraSegundaNota, float *MediaTotal, float *TerceiraNota)
 {
-	*MediaTotal = (*nota1 + *nota2 + *nota3)/3;
-	
-	if(*nota1 > *nota2)
-		*MediaPrimeiraSegundaNota = *MediaPrimeiraSegundaNota + nota1;
-	else
-		*MediaPrimeiraSegundaNota = *MediaPrimeiraSegundaNota + nota2;
-	
-	if(*nota2 > *nota3)
-		*MediaPrimeiraSegundaNota = *MediaPrimeiraSegundaNota + nota2;
-	else
-		*MediaPrimeiraSegundaNota = *MediaPrimeiraSegundaNota + nota3;
-		
-	*MediaPrimeiraSegundaNota = *MediaPrimeiraSegundaNota / 2;
-	
-	
-	
+        *MediaTotal = (*nota1 + *nota2 + *nota3)/3;
+
+        if (*nota1 >= *nota2 && *nota1 >= *nota3)
+    {
+        *PrimeiraNota = *nota1;
+
+        if (*nota2 >= *nota3)
+        {
+            *SegundaNota = *nota2;
+            *TerceiraNota = *nota3;
+        }
+        else
+        {
+            *SegundaNota = *nota3;
+            *TerceiraNota = *nota2;
+        }
+    }
+    else if (*nota2 >= *nota1 && *nota2 >= *nota3)
+    {
+        *PrimeiraNota = *nota2;
+
+        if (*nota1 >= *nota3)
+        {
+            *SegundaNota = *nota1;
+            *TerceiraNota = *nota3;
+        }
+        else
+        {
+            *SegundaNota = *nota3;
+            *TerceiraNota = *nota1;
+        }
+    }
+    else
+    {
+        *PrimeiraNota = *nota3;
+
+        if (*nota1 >= *nota2)
+        {
+            *SegundaNota = *nota1;
+            *TerceiraNota = *nota2;
+        }
+        else
+        {
+            *SegundaNota = *nota2;
+            *TerceiraNota = *nota1;
+        }
+    }
+
+        *MediaPrimeiraSegundaNota = (*PrimeiraNota+*SegundaNota) / 2;
+
+
+
 }
